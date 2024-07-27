8961dcaf69e100368595cee76287e2a574f5e481
nikita-r <gfgfgf354@gmail.com>
2024-07-27 13:25:19 +0300

    Created the ad and author models. Migrated the tables.

--- src/handlerapi/models.py
+++ src/handlerapi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class AuthorAdvertisement(models.Model):
+    author_title = models.CharField(max_length=255)
+
+
+class Advertisement(models.Model):
+    ad_title = models.CharField(max_length=255)
+    ad_id = models.IntegerField()
+    ad_author = models.ForeignKey(AuthorAdvertisement, on_delete=models.CASCADE, related_name='advertisements')
+    ad_views = models.IntegerField()
+    ad_position = models.IntegerField()
+
